4d001c9d7371c1fcdaee2c17854ad4e4af084cbf
YoussefHatem-cds <yousefsaied578@gmail.com>
2025-02-24 16:28:36 +0200

    fixed draw score problem,changing score to struct object

--- RockScissorsPaper/RockScissorsPaper.cpp
+++ RockScissorsPaper/RockScissorsPaper.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std;
+
 
 enum Enchoices {
     paper = 1,
@@ -7,14 +7,34 @@ enum Enchoices {
     rock = 3
 };
 
-int readposstive(string messege) {
-    int num;
+struct stScore
+{
 
+    //int Player_score = 0; int PC_score = 0; int Ties = 0; int rounds = 0; int r;
+
+    int Player_score=0;
+    int PC_score=0;
+    int ties=0;
+    
+
+};
+using namespace std;
+
+int readposstive(string messege,int start,int limit) {
+    int num; 
     cout << messege << endl;
     cin >> num;
 
-    return num;
+    while (cin.fail() || (num<start||num>limit)) {
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
+        cout << "Invalid input please try again\n\n";
+        cout << messege;
+        cin >> num;
+    }
+
+    return num;
 }
 
 int RandomNumber(int From, int To)
@@ -22,19 +42,15 @@ int RandomNumber(int From, int To)
     //Function to generate a random number
     int randNum = rand() % (To - From + 1) + From;
     return randNum;
-
 }
 
 int ReadRounds() {
     cout << "How many rounds do u want to play [1-10]\n";
-    int rounds = readposstive("Rounds:");
+    int rounds = readposstive("Rounds:",1,10);
     return rounds;
 }
 
-int pc() {
-    int choice = RandomNumber(0, 3);
-    return choice;
-}
+int pc() {return  RandomNumber(0, 3);}
 
 int logic(Enchoices player, Enchoices pc) {
     if (player == pc) { return 2; }
@@ -50,13 +66,6 @@ int logic(Enchoices player, Enchoices pc) {
 
 }
 
-Enchoices ReadChoice(int choice) {
-
-
-    return (Enchoices)choice;
-
-}
-
 int checkWinner(int result) {
     int winner = 0;
 
@@ -64,14 +73,14 @@ int checkWinner(int result) {
     {
     case 0:
         system("Color 47");
-        cout << "========PC win this round GOOD LUCK========\n ";
+        cout << "========PC Won this round GOOD LUCK========\n ";
         winner = 0;
         break;
 
     case 1:
         cout << "\a";
         system("Color 20");
-        cout << "========You Won BUT dont get too excited========\n";
+        cout << "========player Won this round good job========\n";
         winner = 1;
         break;
 
@@ -91,7 +100,6 @@ int checkWinner(int result) {
 void printElemtns() {
 
     cout << "Pick number [1,2,3] to select your choice.\n\n";
-
     cout << "Paper   : 1\n";
     cout << "Scissors: 2\n";
     cout << "Rock    : 3\n\n";
@@ -124,7 +132,7 @@ string getChoice(int choice) {
 
 }
 
-void printResult(string player, string PC) {
+void printChoice(string player, string PC) {
     cout << "Your choice :" << player << endl;
     cout << "PC choice   :" << PC << endl;
 
@@ -144,72 +152,63 @@ void PrintSummery(int& Player_score, int& PC_score, int& Ties, int rounds) {
     cout << "-------------------------------------------\n";
     cout << "---------------summery---------------------\n";
     cout << "---------------       ---------------------\n";
-
     cout << "----------Game rounds    : " << rounds << "---------------" << endl;
     cout << "----------Player Won time: " << Player_score << "---------------" << endl;
     cout << "----------PC Won time    : " << PC_score << "---------------" << endl;
     cout << "----------Draw Won time  : " << Ties << "---------------" << endl;
-
     cout << "-------------------------------------------\n";
     cout << "-------------------------------------------\n";
     cout << "-------------------------------------------\n";
-
-    if (Player_score > PC_score) {
-        system("Color 20");
-        cout << "Final winner : Player" << endl;
-    }
-    else {
-        system("Color 47");
-        cout << "Final winner : PC" << endl;
-    }
-
+    cout << "\t";
+    if (Player_score > PC_score) 
+    {system("Color 20"); cout << "FINAL WINNER: Player" << endl;}
+    else if(PC_score>Player_score)
+    {system("Color 47"); cout << "FINAL WINNER : PC" << endl;}
+    else{ system("Color 0f"); cout << "its a tie there is no winner" << endl; }
+    cout << endl;
 }
 
 void startgame() {
-    int cont = 0;
-    int Player_score = 0; int PC_score = 0; int Ties = 0; int rounds = 0; int r;
+    int cont = 0; int rounds = 0;
     do {
+        //clear all text in pervoius round and rest color to black
         system("cls");
         system("Color 0f");
 
-        Player_score = 0;  PC_score = 0;  Ties = 0;
-
+        //reset scores to zero
+        stScore scores;
+        
+        //read rounds from user
         int rounds = ReadRounds();
-        r = rounds;
-        int round = 1;
+        int round = 1;  //counter
+
         while (round <= rounds) {
 
             cout << "Round [" << round << "] begins\n";
-            printElemtns();
-            int target = readposstive("Enter a number:  ");
-            int PCtarget = pc();
-            Enchoices choice = ReadChoice(target);
-            Enchoices PCchoice = ReadChoice(PCtarget);
+            printElemtns();                         //print the choices to user
+            int target = readposstive("Enter a number: ",1,3);
+            int PCtarget = pc();                    //generate random choice to pc
+            Enchoices choice = (Enchoices)(target);
+            Enchoices PCchoice = (Enchoices)(PCtarget);
 
-            int result = logic(choice, PCchoice);
+            int result = logic(choice, PCchoice);   //check the winner 0:pc,1:player,2:tie
 
-            string player = getChoice(target);
+            string player = getChoice(target);      //get player,pc choice to string
             string PC = getChoice(PCtarget);
 
-            printResult(player, PC);
+            printChoice(player, PC);                //print both player and pc choice 
 
-            int Winner = checkWinner(result);
-
-            Scores(Winner, Player_score, PC_score, Ties);
-
-            round++;
-
+            int Winner = checkWinner(result);       //check the winner and print 
+            Scores(Winner, scores.Player_score, scores.PC_score, scores.ties);                                                //increase the winner score
+            round++;                                //increment to next round
 
         }
 
-        PrintSummery(Player_score, PC_score, Ties, r);
-
-
-        cout << "Play again??? 0 for no , 1 for yes\n";
-        cin >> cont;
+        //print the rounds summery after all rounds finished 
+        PrintSummery(scores.Player_score, scores.PC_score, scores.ties, rounds);
+        cont=readposstive("Play again ? ? ? 0 for no, 1 for yes\n",0,1);
 
     } while (cont == 1);
-
 
 }
 
